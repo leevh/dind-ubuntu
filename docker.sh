f39413642df40024594c6d4f91dc1af0c42e94ae
Lee vanHalderen <lee.vanhalderen@viu.ca>
2017-03-16 12:37:39 -0700

    removed overlay

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec dockerd --storage-driver=overlay2 >>/var/log/docker.log 2>&1
+exec dockerd --storage-driver=overlay >>/var/log/docker.log 2>&1
